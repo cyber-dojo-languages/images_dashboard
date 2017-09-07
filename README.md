7f0968a495556aeef374491ecd2da958539d21e0
Jon Jagger <jon@jaggersoft.com>
2017-09-07 07:53:24 +0100

    curl the json data to get all the repo info

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 A simple [ruby](https://www.ruby-lang.org/en/) app for displaying
 the build status of all cyber-dojo-languages repos.
+Runs locally, or inside a docker-container.
--- app/src/images_dashboard.rb
+++ app/src/images_dashboard.rb
@@ -1,10 +1,29 @@
 require 'sinatra/base'
 require 'json'
+require_relative 'assert_system'
 
 class ImagesDashboard < Sinatra::Base
 
   get '/' do
+    @json = curled_triples
     erb :home
+  end
+
+  private
+
+  include AssertSystem
+
+  def curled_triples
+    assert_system "curl --silent -o /tmp/#{triples_filename} #{triples_url}"
+    JSON.parse(IO.read("/tmp/#{triples_filename}"))
+  end
+
+  def triples_url
+    "https://raw.githubusercontent.com/cyber-dojo-languages/images_info/master/#{triples_filename}"
+  end
+
+  def triples_filename
+    'images_info.json'
   end
 
 end
