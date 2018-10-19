c0c7d93676c1ee049326f55c53c40ea8b937d48c
Jon Jagger <jon@jaggersoft.com>
2018-10-19 11:23:51 +0100

    refactoring

--- app/src/public/js/images-dashboard.js
+++ app/src/public/js/images-dashboard.js
@@ -9,23 +9,26 @@ window.reload = function(id,org) {
     $('td span.took',  tr).html('');
   });
 
+  const html_state = (value) => {
+    let colour = '';
+    switch (value) {
+      case 'failed':
+        colour = 'red'; break;
+      case 'passed':
+        colour = 'green'; break;
+      default:
+        colour = 'black'; break;
+    }
+    return '<span style="color:'+colour+';">'+value+'</span>';
+  };
+
   $(id + ' tr[name]').each(function() {
     const tr = $(this);
     const repo = tr.attr('name');
     $.ajax({
       url: '/build?org='+org+'&repo='+repo,
       success: function(build) {
-        var colour, html;
-        switch (build.state) {
-          case 'failed':
-            colour = 'red'; break;
-          case 'passed':
-            colour = 'green'; break;
-          default:
-            colour = 'black'; break;
-        }
-        html = '<span style="color:'+colour+';">'+build.state+'</span>';
-        $('td span.state', tr).html(html);
+        $('td span.state', tr).html(html_state(build.state));
         $('td span.age',   tr).html(build.age);
         $('td span.took',  tr).html(build.took);
       }
